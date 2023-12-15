f653dd71673fb9255cdd023aaae98996ef66388d
Tony Andy Oehme <go98mub@mytum.de>
2023-12-15 23:19:24 +0100

    mergeSort

---
+++ src/SortingAlgorithms.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+
+public class SortingAlgorithms {
+    public static int[] merge(int[] one, int[] two) {
+
+        int[] sorted = new int[one.length + two.length];
+
+        int onePointer, twoPointer, sortedPointer;
+        onePointer = twoPointer = sortedPointer = 0;
+
+        while(onePointer < one.length && twoPointer < two.length){
+            if(one[onePointer] <= two[twoPointer]){
+                sorted[sortedPointer++] = one[onePointer++];
+            } else {
+                sorted[sortedPointer++] = two[twoPointer++];
+            }
+        }
+
+
+        while(twoPointer < two.length){
+            sorted[sortedPointer++] = two[twoPointer++];
+        }
+
+        while(onePointer < one.length){
+            sorted[sortedPointer++] = one[onePointer++];
+        }
+
+        return sorted;
+
+    }
+
+    public static int[] mergeSort(int[] arrayToSort) {
+
+        System.out.println(Arrays.toString(arrayToSort));
+
+        if(arrayToSort.length < 2){
+            return arrayToSort;
+        }
+
+        int middle = arrayToSort.length / 2;
+
+        int[] rightHalf = mergeSort(Arrays.copyOfRange(arrayToSort, 0, middle));
+        int[] leftHalf = mergeSort(Arrays.copyOfRange(arrayToSort, middle, arrayToSort.length));
+
+        return merge(rightHalf, leftHalf);
+    }
+}
